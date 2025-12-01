80a4817a82a10d31e2016307a36ac12c3071975b
sankalp-singh-07 <sankalp.singh.somvanshi@gmail.com>
2025-12-01 15:21:10 +0530

    New Backend Common package created, exported and imported in http backend along with a few routes

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,6 +1,22 @@
 import express from 'express'
+import jwt from 'jsonwebtoken';
+import {JWT_SECRET} from "@repo/backend-common/config"
 
 const app = express();
+
+app.post('/signin', (req, res) => {
+
+})
+
+app.post('/signup', (req, res) => {
+    const {userId, name} = req.body;
+
+    const token = jwt.sign({userId}, JWT_SECRET)
+})
+
+app.post('/room', (req, res) => {
+
+})
 
 app.get('/',(req, res) => {
     res.send("Hello world") 
